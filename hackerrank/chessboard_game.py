bb144cea96245b07ecda9a323637b19bbe38eba7
santhosh205 <santhoshdasari205@gmail.com>
2017-08-06 04:02:45 +0530

    2nd Problem

---
+++ hackerrank/chessboard_game.py
@@ -0,0 +1,26 @@
+# https://www.hackerrank.com/challenges/a-chessboard-game-1
+# checkout - https://www.hackerrank.com/challenges/a-chessboard-game-1/forum/comments/208350
+# checkout jawad_cs's comment in discussions
+# checkout peterekras's comment in discussions 
+
+x = [[]]
+def checkAllMoves(x, y):
+    if x<1 or y<1 or x>15 or y>15:
+        return 1
+    if store[x-1][y-1] != -1:
+        return store[x-1][y-1]
+    if (checkAllMoves(x-2, y+1) and checkAllMoves(x-2, y-1) and checkAllMoves(x+1, y-2) and checkAllMoves(x-1, y-2)):
+        store[x-1][y-1] = 0
+        return 0
+    else:
+        store[x-1][y-1] = 1
+        return 1
+poses = [[5, 2], [5, 3], [8, 8]]
+for pos in poses:
+    x, y = pos[0], pos[1]
+    store = [[-1 for x in range(15)] for y in range(15)]
+    outcome = checkAllMoves(x, y)
+    if outcome == 0:
+        print("Second")
+    else:
+        print("First")
--- hackerrank/coin_change.py
+++ hackerrank/coin_change.py
@@ -1,3 +1,6 @@
+# https://www.hackerrank.com/challenges/coin-change
+# checkout - http://www.geeksforgeeks.org/dynamic-programming-set-7-coin-change/
+
 coins = [1, 2, 3]
 n = 5
 ways = [0 for k in range(n+1)]
